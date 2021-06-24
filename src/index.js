1b65c179ffa0ef251fc4eb600215d18a84f6b34a
root <root@DESKTOP-ECJHP9N.localdomain>
2021-06-24 12:35:27 -0300

    ESLint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
-import Fibonacci from './components/Fibonacci';
-import Number from './components/Number';
 
-ReactDOM.render(<Number number="3" />, document.getElementById('app'));
+ReactDOM.render(<HelloWorld number="3" />, document.getElementById('app'));
